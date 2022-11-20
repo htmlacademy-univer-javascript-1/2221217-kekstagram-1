d5378379c88bdf6f43022d8ffec7299c9aebb4cb
Keks <keks@htmlacademy.ru>
2022-11-21 02:30:31 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/pictures.js
@@ -0,0 +1,28 @@
+import {renderBigPicture} from './big-picture.js';
+
+const storagePictures = document.querySelector('.pictures');
+const layoutPicture = document.querySelector('#picture')
+.content
+.querySelector('.picture');
+
+function createPicture(pictureInformation) {
+  const picture = layoutPicture.cloneNode(true);
+  picture.querySelector('.picture__img').src = pictureInformation.url;
+  picture.querySelector('.picture__likes').textContent = pictureInformation.likes;
+  picture.querySelector('.picture__comments').textContent = pictureInformation.comments.length;
+  picture.addEventListener('click', () => {
+    renderBigPicture(pictureInformation);
+  });
+  return picture;
+}
+
+function renderPictures(picturesInformation) {
+  const similarListPictureFragment = document.createDocumentFragment();
+  picturesInformation.forEach((pictureInformation) => {
+    const picture = createPicture(pictureInformation);
+    similarListPictureFragment.appendChild(picture);
+  });
+  storagePictures.appendChild(similarListPictureFragment);
+}
+
+export {renderPictures};
