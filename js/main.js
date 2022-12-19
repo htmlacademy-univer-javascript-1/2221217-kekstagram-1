21f2809b2765beb85c395a658e06934443733f24
Михаил <gmike103g@gmail.com>
2022-12-20 04:31:42 +0500

    Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -1,19 +1,11 @@
 import {renderPictures} from './pictures.js';
 import {getData} from './api.js';
+import {loadingDataError} from './util.js';
+import {addFilteringButtons} from './picture-filter.js';
 import './upload-picture-form.js';
 import './picture-effect.js';
 
-const body = document.querySelector('body');
-
-function loadingDataError() {
-  const templateMessageLoadingDataError = document.querySelector('#error').content.querySelector('section');
-  const messageLoadingDataError = templateMessageLoadingDataError.cloneNode(true);
-  messageLoadingDataError.querySelector('h2').textContent = 'Ошибка загрузки данных';
-  messageLoadingDataError.querySelector('button').remove();
-  body.append(messageLoadingDataError);
-  setTimeout(() => {
-    body.removeChild(messageLoadingDataError);
-  }, 5000);
-}
-
-getData(renderPictures, loadingDataError);
+getData((pictureInformation) => {
+  renderPictures(pictureInformation);
+  addFilteringButtons(pictureInformation);
+}, loadingDataError);
--- js/util.js
+++ js/util.js
@@ -1,3 +1,16 @@
+const body = document.querySelector('body');
+
+function loadingDataError() {
+  const templateMessageLoadingDataError = document.querySelector('#error').content.querySelector('section');
+  const messageLoadingDataError = templateMessageLoadingDataError.cloneNode(true);
+  messageLoadingDataError.querySelector('h2').textContent = 'Ошибка загрузки данных';
+  messageLoadingDataError.querySelector('button').remove();
+  body.append(messageLoadingDataError);
+  setTimeout(() => {
+    body.removeChild(messageLoadingDataError);
+  }, 5000);
+}
+
 function getRandomPositiveInteger(a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -5,9 +18,31 @@ function getRandomPositiveInteger(a, b) {
   return Math.floor(result);
 }
 
-function getRandomArrayElement(elements)  {
-  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+function getRandomArrayElements(elements, count)  {
+  const newArray = [];
+  while (newArray.length < count) {
+    const newElement = elements[getRandomPositiveInteger(0, elements.length - 1)];
+    if (!newArray.includes(newElement)) {
+      newArray.push(newElement);
+    }
+  }
+  return newArray;
 }
 
-export {getRandomPositiveInteger};
-export {getRandomArrayElement};
+function pictureInformationSortingMethod(pictureInformation1, pictureInformation2) {
+  return pictureInformation2.comments.length - pictureInformation1.comments.length;
+}
+
+function getSortedPictureInformation(pictureInformation) {
+  return pictureInformation.slice().sort(pictureInformationSortingMethod);
+}
+
+function debounce (callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+export {loadingDataError, getRandomArrayElements, getSortedPictureInformation, debounce};
